8b6d67ae9789e05f345b759253b8a85e58cca589
seung064 <gli056@naver.com>
2025-07-26 23:00:08 +0900

    Update : Models

---
+++ POP_Project/Models/AppLoginInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POP_Project.Models
+{
+    public class AppLoginInfo
+    {
+        public static string CurrentUserId { get; set; }
+        public static string CurrentUserPwd { get; set; }
+        public static string CurrentUserName { get; set; }
+    }
+}
