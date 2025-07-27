53182a6a547c5d0c2987a4e83109896e848aa9cb
HYUN-SU123 <gustn8608@naver.com>
2025-07-27 15:49:41 +0900

    XAML 통합
    통합

---
+++ POP_Project/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Diagnostics;
+
+namespace POP_Project
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.DispatcherUnhandledException += (s, e) =>
+            {
+                // MessageBox.Show(e.Exception.Message);
+                e.Handled = true;
+            };
+
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            StartDectorProcess(); // 프로그램 시작 시 자동 실행
+
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+
+        Process detectorProcess = null;
+        public void StartDectorProcess()
+        {
+            detectorProcess = new Process();
+            detectorProcess.StartInfo.FileName = "python";
+            detectorProcess.StartInfo.Arguments = "OpenCV_Detector.py";
+            detectorProcess.StartInfo.WorkingDirectory = @".";
+            detectorProcess.Start();
+            MessageBox.Show("실행완료");
+        }
+    }
+}
